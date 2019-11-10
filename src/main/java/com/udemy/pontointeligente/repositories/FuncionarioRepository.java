8959af6be4c1a567df378581f50e2d6b4cae8cca
viniciosi <vinicio_si@yahoo.com.br>
2019-11-10 11:46:59 -0300

    atualização

---
+++ src/main/java/com/udemy/pontointeligente/repositories/FuncionarioRepository.java
@@ -0,0 +1,17 @@
+package com.udemy.pontointeligente.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.udemy.pontointeligente.entities.Funcionario;
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findByCpf(String cpf);
+
+	Funcionario findByEmail(String email);
+
+	Funcionario findByCpfOrEmail(String cpf, String email);
+
+}
